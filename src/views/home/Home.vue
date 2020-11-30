8a463e087451518ce21ec745c3463ded8eca87e5
闫彪 <1536696390@qq.com>
2020-11-30 20:46:05 +0800

    初始化项目

---
+++ src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+   <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import RecommendView from './childComps/RecommendView'
+
+
+import { getHomeMultidate } from 'network/home'
+
+
+
+export default {
+  components: {
+    NavBar,
+ 
+    RecommendView
+    
+   
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidate().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style>
+.home-nav {
+  background: var(--color-tint);
+  color: white;
+}
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.num" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.recommend {
+  display: flex;
+  text-align: center;
+  padding: 15px 0 30px;
+  border-bottom: 8px solid #eee;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 65px;
+  height: 65px;
+}
+</style>
